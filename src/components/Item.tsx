ad22cf8b2c0480b8bb4cf87c40cbd01f805d24e1
Matthew <matthewridgway.deximo@gmail.com>
2024-03-07 23:40:53 +0800

    WIP Fixed component structure

--- src/components/Item.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from "react";
-
-type ItemProps = {
-    children: React.ReactNode;
-};
-
-export default function Item({ children }: ItemProps) {
-    return <li className="item">{children}</li>;
-}
