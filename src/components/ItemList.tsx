541cfd4f03156042976fd2e7a9bf7ef5b0b8d866
Matthew <matthewridgway.deximo@gmail.com>
2024-03-09 21:45:33 +0800

    WIP Refactored components

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,12 +1,12 @@
 import UnorderedList from "./UnorderedList";
 
-type InitialItemList = {
+type Item = {
     id: number;
     name: string;
     isCompleted: boolean;
-}[];
+};
 
-const initialItemList: InitialItemList = [
+const initialItemList: Item[] = [
     { id: 1, name: "good mood", isCompleted: true },
     { id: 2, name: "passport", isCompleted: false },
     { id: 3, name: "phone charger", isCompleted: false },
