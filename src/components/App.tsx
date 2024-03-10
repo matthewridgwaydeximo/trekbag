969210b0246b5923c672a83fa58753e3f2ef460e
Matthew <matthewridgway.deximo@gmail.com>
2024-03-10 21:13:56 +0800

    WIP Implemented add item to the list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
 import Main from "./Main";
+import { INITIAL_ITEM_LIST } from "../lib/constants";
 
 export default function App() {
+    const [items, setItems] = useState(INITIAL_ITEM_LIST);
+
+    const handleAddItem = (name: string) => {
+        const item = {
+            id: new Date().getMilliseconds(),
+            name: name,
+            isCompleted: false,
+        };
+
+        setItems((prev) => [item, ...prev]);
+    };
+
     return (
         <>
             <BackgroundHeading />
-            <Main />
+            <Main items={items} handleAddItem={handleAddItem} />
             <Footer />
         </>
     );
