5b2efbf94ff86e528da2b42e12109c26a9a1c39c
Matthew <matthewridgway.deximo@gmail.com>
2024-03-11 23:02:34 +0800

    WIP Finished secondary button actions TODO Make a useSidebarButtons hook

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,19 +4,4 @@ export const INITIAL_ITEM_LIST: TItem[] = [
     { id: 1, name: "good mood", isCompleted: true },
     { id: 2, name: "passport", isCompleted: false },
     { id: 3, name: "phone charger", isCompleted: false },
-];
-
-export const BUTTON_GROUP_OPTIONS_TYPE = [
-    {
-        text: "Mark all as complete",
-    },
-    {
-        text: "Mark all as incomplete",
-    },
-    {
-        text: "Reset to initial",
-    },
-    {
-        text: "Remove all items",
-    },
-];
+] as const;
